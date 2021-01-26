f7f683df35e726641f7c534581c52c52e501f81e
stephen <mqhbeijing@163.com>
2021-01-26 16:25:27 +0800

    config

--- ratel-mybatis-spring-boot-autoconfigure/src/main/java/org/ratel/mybatis/spring/boot/autoconfigure/RatelMybatisAutoConfiguration.java
+++ ratel-mybatis-spring-boot-autoconfigure/src/main/java/org/ratel/mybatis/spring/boot/autoconfigure/RatelMybatisAutoConfiguration.java
@@ -1,12 +1,17 @@
 package org.ratel.mybatis.spring.boot.autoconfigure;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * mybatis配置类
  *
  * @author stephen
  * @date 2021/1/26 1:18 下午
  */
+@Configuration
+@EnableConfigurationProperties(RatelMybatisProperties.class)
 public class RatelMybatisAutoConfiguration {
-    
+
 
 }
---
+++ ratel-mybatis-spring-boot-autoconfigure/src/main/java/org/ratel/mybatis/spring/boot/autoconfigure/RatelMybatisProperties.java
@@ -0,0 +1,12 @@
+package org.ratel.mybatis.spring.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author stephen
+ * @date 2021/1/26 4:17 下午
+ */
+@ConfigurationProperties("ratel.mybatis")
+public class RatelMybatisProperties {
+
+}
