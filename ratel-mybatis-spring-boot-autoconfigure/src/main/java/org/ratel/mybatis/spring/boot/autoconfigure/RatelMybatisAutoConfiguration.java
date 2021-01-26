23e0f6738a1437973144d391e6eb189dea084207
mengqinghao <mengqinghao@finupgroup.com>
2021-01-26 13:34:42 +0800

    init

---
+++ ratel-mybatis-spring-boot-autoconfigure/src/main/java/org/ratel/mybatis/spring/boot/autoconfigure/RatelMybatisAutoConfiguration.java
@@ -0,0 +1,11 @@
+package org.ratel.mybatis.spring.boot.autoconfigure;
+
+/**
+ * mybatis配置类
+ *
+ * @author stephen
+ * @date 2021/1/26 1:18 下午
+ */
+public class RatelMybatisAutoConfiguration {
+
+}
